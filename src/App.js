d9ca73f4b44c7555dc45bbb34a9bc963f5a80705
Jan-Philipp Winkler <janphilipp.winkler@gmail.com>
2023-12-13 11:56:09 +0100

    challenge done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React from "react";
 import "./styles.css";
 
+function HelloWorld() {
+  return <h1>Hello world!</h1>;
+}
+
 export default function App() {
-  return <div>Say hello...</div>;
+  return <HelloWorld />;
 }
